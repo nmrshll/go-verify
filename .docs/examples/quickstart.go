e29f52658758a6b83b23fd7a025dd47225b7d399
nmrshll <marshall.nicolas@gmail.com>
2018-09-22 01:05:21 +0200

    cleanup

--- .docs/examples/quickstart.go
+++ .docs/examples/quickstart.go
@@ -24,37 +24,6 @@ func FunctionToCall(arg1 Argument, arg2 *Argument) error {
 	return nil
 }
 
-// func FunctionToCall(arg1 Argument, arg2 *Argument) error {
-// 	err := verify.That(string(arg1) != "", "arg 1 can't be empty")
-// 	err = verify.That(arg2 != nil, "arg2 can't be nil")
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = verify.That(*arg2 == Argument("hello world"), "arg2 must be \"hello world\"")
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// perform your function here
-
-// 	return nil
-// }
-
-// func FunctionToCall(arg1 Argument, arg2 *Argument) error {
-// 	if err := verify.All(
-// 		verify.That(string(arg1) != "", "arg 1 can't be empty"),
-// 		verify.That(arg2 != nil, "arg2 can't be nil"),
-// 		verify.That(*arg2 == Argument("hello world"), "arg2 must be \"hello world\""),
-// 	); err != nil {
-// 		return err
-// 	}
-
-// 	// perform your function here
-
-// 	return nil
-// }
-
 func main() {
 	arg1 := Argument("hola mundo")
 	arg2 := Argument("hola mundo") // incorrect argument as "hello world is expected here"
--- verify.go
+++ verify.go
@@ -23,34 +23,16 @@ func verifierForWrappingPC(pc uintptr) (_ *verifier, upFunctionLocationUint uint
 	return verifiers[upFunctionLocationUint], upFunctionLocationUint
 }
 
-// type verification struct {
-// 	condition    bool
-// 	errorMessage string
-// }
-
-// type verifFunc func() verification
-
-// func That(condition bool, errorMessage string) verifFunc {
-// 	return func() verification {
-// 		return verification{condition, errorMessage}
-// 	}
-// }
-
 func That(condition bool, errorFormat string, args ...interface{}) {
 	pc, _, _, ok := runtime.Caller(1)
 	if !ok {
-		// return fmt.Errorf("failed evaluating runtime.caller")
 		panic(fmt.Errorf("failed evaluating runtime.caller"))
 	}
-	// function := runtime.FuncForPC(pc)
 	verifier, _ := verifierForWrappingPC(pc)
-	// spew.Dump(verifier.Error())
-	// // runtime.FuncForPC(pc)
+
 	if !condition {
-		// return fmt.Errorf(errorFormat, args...)
 		verifier.err = fmt.Errorf(errorFormat, args...)
 	}
-	// return nil
 }
 
 func Error() error {
@@ -68,34 +50,3 @@ func Error() error {
 
 	return verifier.Error()
 }
-
-// func All(verifFuncs ...verifFunc) error {
-// 	for _, verifFunc := range verifFuncs {
-// 		v := verifFunc()
-// 		if !v.condition {
-// 			return stacktrace.NewError(v.errorMessage)
-// 		}
-// 	}
-// 	return nil
-// }
-
-// func StringValueOf(in *string) (out string) {
-// 	if in == nil {
-// 		return
-// 	}
-// 	return *in
-// }
-
-// func IntValueOf(in *int) (out int) {
-// 	if in == nil {
-// 		return
-// 	}
-// 	return *in
-// }
-
-// func I64ValueOf(in *int64) (out int64) {
-// 	if in == nil {
-// 		return
-// 	}
-// 	return *in
-// }
